6d2879a8786664bdaaad75079dd897f76a2a4637
Haramchu <clementsamuelm@gmail.com>
2025-02-10 13:58:27 +0700

    feat:init Springboot and Dependencies

---
+++ src/main/java/propensi/amesta/AmestaApplication.java
@@ -0,0 +1,13 @@
+package propensi.amesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/propensi/amesta/AmestaApplicationTests.java
@@ -0,0 +1,13 @@
+package propensi.amesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
