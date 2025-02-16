c684c1d1adeeb5988b11224acfc0a783b488cd33
Haramchu <clementsamuelm@gmail.com>
2025-02-16 19:33:30 +0700

    feat: add komisaris model and dummy account

--- src/main/java/propensi/amesta/AmestaApplication.java
+++ src/main/java/propensi/amesta/AmestaApplication.java
@@ -13,6 +13,11 @@ import propensi.amesta.repository.EndUser.UserDb;
 import propensi.amesta.service.UserService;
 import propensi.amesta.model.EndUser.User;
 import propensi.amesta.model.EndUser.Administrasi;
+import propensi.amesta.model.EndUser.Direktur;
+import propensi.amesta.model.EndUser.Sales;
+import propensi.amesta.model.EndUser.GeneralManager;
+import propensi.amesta.model.EndUser.KepalaGudang;
+
 
 @SpringBootApplication
 public class AmestaApplication {
@@ -34,6 +39,61 @@ public class AmestaApplication {
 					user.setUsername("admin");
 					user.setPassword(userService.hashPassword("admin"));
 					user.setEmail("admin@example.com");
+					user.setGender(false);
+					user.setCreatedDate(new Date());
+					user.setUpdatedAt(new Date());
+					userDb.save(user);
+				}
+				if (userDb.findByUsername("direktur") != null) {
+					user = new Direktur();
+					user.setName("direktur");
+					user.setUsername("direktur");
+					user.setPassword(userService.hashPassword("direktur"));
+					user.setEmail("direktur@example.com");
+					user.setGender(false);
+					user.setCreatedDate(new Date());
+					user.setUpdatedAt(new Date());
+					userDb.save(user);
+				}
+				if (userDb.findByUsername("sales") != null) {
+					user = new Sales();
+					user.setName("sales");
+					user.setUsername("sales");
+					user.setPassword(userService.hashPassword("sales"));
+					user.setEmail("sales@example.com");
+					user.setGender(false);
+					user.setCreatedDate(new Date());
+					user.setUpdatedAt(new Date());
+					userDb.save(user);
+				}
+				if (userDb.findByUsername("general_manager") != null) {
+					user = new GeneralManager();
+					user.setName("general_manager");
+					user.setUsername("general_manager");
+					user.setPassword(userService.hashPassword("general_manager"));
+					user.setEmail("gm@example.com");
+					user.setGender(false);
+					user.setCreatedDate(new Date());
+					user.setUpdatedAt(new Date());
+					userDb.save(user);
+				}
+				if (userDb.findByUsername("kepala_gudang") != null) {
+					user = new KepalaGudang();
+					user.setName("kepala_gudang");
+					user.setUsername("kepala_gudang");
+					user.setPassword(userService.hashPassword("kepala_gudang"));
+					user.setEmail("kg@example.com");
+					user.setGender(false);
+					user.setCreatedDate(new Date());
+					user.setUpdatedAt(new Date());
+					userDb.save(user);
+				}
+				if (userDb.findByUsername("komisaris") != null) {
+					user = new Administrasi();
+					user.setName("komisaris");
+					user.setUsername("komisaris");
+					user.setPassword(userService.hashPassword("komisaris"));
+					user.setEmail("komisaris@example.com");
 					user.setGender(false);
 					user.setCreatedDate(new Date());
 					user.setUpdatedAt(new Date());
