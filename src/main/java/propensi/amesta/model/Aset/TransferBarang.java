1bb47cf91bbc96859af6e2b4429d2dd5533a109b
unknown <michaelmarcellino54@gmail.com>
2025-03-14 20:54:47 +0700

    feat:add model transfer barang

--- src/main/java/propensi/amesta/model/Aset/TransferBarang.java
+++ src/main/java/propensi/amesta/model/Aset/TransferBarang.java
@@ -1,11 +1,19 @@
 package propensi.amesta.model.Aset;
 
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "transfer_barang")
 public class TransferBarang {
@@ -13,4 +21,20 @@ public class TransferBarang {
     @Id
     private String id;
 
+    @NotNull(message = "Nama Barang harus diisi")
+    private String namaBarang;
+
+    @NotNull(message = "Alamat gudang harus diisi")
+    private String gudangTujuan;
+
+    @NotNull(message = "Alamat gudang harus diisi")
+    private String gudangAsal;
+
+    @NotNull(message = "Tipe Proses harus dipilih")
+    private int tipeProses; // 0 = Kirim, 1 = Terima
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "createdDate", updatable = false, nullable = false)
+    private Date createdDate;
 }
