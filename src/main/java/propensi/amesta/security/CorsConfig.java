c9be086a59d972731362f91eeee0481f1c5242dd
Haramchu <clementsamuelm@gmail.com>
2025-03-25 18:01:07 +0700

    feat: add second vercel web

--- src/main/java/propensi/amesta/security/CorsConfig.java
+++ src/main/java/propensi/amesta/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:3002", "https://amsesman-staging.vercel.app")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3002", "https://amsesman-staging.vercel.app", "https://amerta-frontend-six.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
